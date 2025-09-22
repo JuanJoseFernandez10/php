82e36fed7a06c0169d9144113d437d0719c120db
JuanJoseFernandez10 <fernandez.suarez.juan.jose@iescamas.es>
2025-09-22 10:30:04 +0200

    Ejercicio 2 Datos personales PHP

---
+++ Ejercicios/ejercicio2.php
@@ -0,0 +1,6 @@
+<?php
+	$nombre = "JuanJose";
+	$direccion = "Calle Huelva blq 2";
+		$numeor = "4343422342";
+		echo $"nombre: " . $nombre . "</br>" . "direccion" . $direccion . "</br>" . "numero" . $numeor;
+?>
